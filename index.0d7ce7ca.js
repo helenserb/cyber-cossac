234a637f365b3b8aae0eb0fda7391b09b0f66c40
helenserb <110823353+helenserb@users.noreply.github.com>
2022-10-30 14:39:34 +0000

    Deploying to gh-pages from @ helenserb/cyber-cossac@1dbf72fc876ea2fd1022835df5e54b9ec4ba7f91 🚀

---
+++ index.0d7ce7ca.js
@@ -0,0 +1,2 @@
+(()=>{const o={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]"),bodyScrollLock:document.querySelector("body")};document.querySelector("body");function e(){o.modal.classList.toggle("is-hidden"),o.bodyScrollLock.classList.toggle("no-scroll")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)})();
+//# sourceMappingURL=index.0d7ce7ca.js.map
---
+++ index.f867b198.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]"),bodyScrollLock:document.querySelector("body")};function e(){o.modal.classList.toggle("is-hidden"),o.bodyScrollLock.classList.toggle("no-scroll")}document.querySelector("body"),o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.f867b198.js.map
