64c037149810e7e0fa62f902db00061acc0bf4d8
helenserb <serbinenkoifbb@gmail.com>
2022-11-05 09:42:15 +0200

    Js for modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,12 +1,16 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
+    openModalBtn2: document.querySelector('[data-modal-nepo]'),
+    openModalBtn3: document.querySelector('[data-modal-peon]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
     bodyScrollLock: document.querySelector('body'),
   };
   const bodyScrollLock = document.querySelector('body');
   refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn2.addEventListener('click', toggleModal);
+  refs.openModalBtn3.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
