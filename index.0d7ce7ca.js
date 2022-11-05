b527b8688ddb529b9b3c1c4551f0cd36f44523a9
helenserb <110823353+helenserb@users.noreply.github.com>
2022-11-05 07:42:57 +0000

    Deploying to gh-pages from @ helenserb/cyber-cossac@64c037149810e7e0fa62f902db00061acc0bf4d8 🚀

--- index.0d7ce7ca.js
+++
@@ -1,2 +0,0 @@
-(()=>{const o={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]"),bodyScrollLock:document.querySelector("body")};document.querySelector("body");function e(){o.modal.classList.toggle("is-hidden"),o.bodyScrollLock.classList.toggle("no-scroll")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)})();
-//# sourceMappingURL=index.0d7ce7ca.js.map
